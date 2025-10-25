6d097837d154d3ca99e73408b2f9a087e18b0318
Wesley Ingoshe <ingoshewesley@gmail.com>
2025-10-25 11:42:11 +0200

    Name,Age

--- ex1.py
+++ ex1.py
@@ -1,1 +1,8 @@
 # Write a program that takes your name as input and prints a personalized greeting along with a calculation of your birth year.
+# input
+
+name = input("Enter your name: ")
+birth_year = int(input("Birth year: "))
+age = int(2025 - birth_year)
+
+print(f"Servus {name}, {age}.")
